3b95aa8891bbc6adcb84725780d0342118e96720
sohailabbas <sohailabbas.ewit@gmail.com>
2025-11-13 20:17:10 +0530

    first commit

---
+++ app/src/main/java/com/sohailabbas/retrosnake/ui/theme/Theme.kt
@@ -0,0 +1,36 @@
+package com.sohailabbas.retrosnake.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val ColorPalette = lightColorScheme(
+    primary = DarkGreen,
+    onPrimary = DarkGreen,
+    secondary = DarkGreen,
+    background = LightGreen
+)
+
+@Composable
+fun RetroSnakeTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = ColorPalette,
+        typography = Typography,
+        content = content
+    )
+}
